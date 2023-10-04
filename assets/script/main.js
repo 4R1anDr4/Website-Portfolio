0c244e16814a6ce7c7dfb8916640ea2884e977bc
Mohammad Ariandra Ravino <ariandra@MacBook-Pro-Mohammad.local>
2023-10-04 09:23:53 +0700

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,19 @@
+// Toggle & Responsive
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("nav-burger");
+    });
+};
+
+navSlide();
+
+// Clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+
+    }
+};
